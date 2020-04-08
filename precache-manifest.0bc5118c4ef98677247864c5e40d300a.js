b330a091c2737755beb026f5e48621be27b44a5e
Robin Aguilar <robin.aguilar@gmail.com>
2020-04-08 02:22:36 -0500

    Updates

---
+++ precache-manifest.0bc5118c4ef98677247864c5e40d300a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6279d4049347062ef2ae49fad5a453a5",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "8acea6ead0e40472b86a",
+    "url": "/React-Portfolio/static/css/main.f34be11e.chunk.css"
+  },
+  {
+    "revision": "f2c307c54856b85270de",
+    "url": "/React-Portfolio/static/js/2.aa63360c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Portfolio/static/js/2.aa63360c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8acea6ead0e40472b86a",
+    "url": "/React-Portfolio/static/js/main.e9cad46d.chunk.js"
+  },
+  {
+    "revision": "55f8281e41e63d261000",
+    "url": "/React-Portfolio/static/js/runtime-main.a0d97005.js"
+  },
+  {
+    "revision": "f66851ef3b87a2fac622e22935d37738",
+    "url": "/React-Portfolio/static/media/BurgerLogger.f66851ef.png"
+  },
+  {
+    "revision": "163420d5df43d1639d141bfb876e4604",
+    "url": "/React-Portfolio/static/media/EmployeeSummary1.163420d5.png"
+  },
+  {
+    "revision": "e9bde01cd40406a5b7153c007786140d",
+    "url": "/React-Portfolio/static/media/EmployeeSummary2.e9bde01c.gif"
+  },
+  {
+    "revision": "fb3b6097de294fe7b3396892f1fc9b35",
+    "url": "/React-Portfolio/static/media/FitnessTracker1.fb3b6097.png"
+  },
+  {
+    "revision": "5c7ab3dc7667b03e6e3cafa8ae86c46b",
+    "url": "/React-Portfolio/static/media/FitnessTracker2.5c7ab3dc.png"
+  },
+  {
+    "revision": "958344bb383feb872e1555b8ca152134",
+    "url": "/React-Portfolio/static/media/MoodSwing1.958344bb.png"
+  },
+  {
+    "revision": "ac1b5f5b9e96a1bf15f178f6325a5705",
+    "url": "/React-Portfolio/static/media/MoodSwing2.ac1b5f5b.png"
+  },
+  {
+    "revision": "4ad1eda60d4584caaaf82b4722c28921",
+    "url": "/React-Portfolio/static/media/NoteTaker1.4ad1eda6.png"
+  },
+  {
+    "revision": "4ad1eda60d4584caaaf82b4722c28921",
+    "url": "/React-Portfolio/static/media/NoteTaker2.4ad1eda6.png"
+  },
+  {
+    "revision": "2c5c956bad70b69829437a003ddc1470",
+    "url": "/React-Portfolio/static/media/headshot1.2c5c956b.jpeg"
+  },
+  {
+    "revision": "373a628418b93eed5291588a59b4d7ea",
+    "url": "/React-Portfolio/static/media/justCoffee1.373a6284.png"
+  },
+  {
+    "revision": "374bc22342451d2d753d6735c2e202da",
+    "url": "/React-Portfolio/static/media/justCoffee2.374bc223.png"
+  }
+]);
